36c30af5da8f55d207f4673102780a4dd8d0fee2
Tathagat Thapliyal <tathagat.thapliyal@gmail.com>
2020-06-13 21:50:58 +0530

    add trip component made
    Signed-off-by: Tathagat Thapliyal <tathagat.thapliyal@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import React from "react";
 import { Nav } from "./components/Nav";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { AddTrip } from "./components/trip/AddTrip";
 
 export class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      allTrips: [
+        {
+          place: "Delhi",
+          date: "2020-06-06",
+          type: "Club",
+        },
+      ],
+    };
+    this.addTrip = this.addTrip.bind(this);
+  }
+
+  addTrip(newTrip) {
+    this.setState((prevState) => {
+      return {
+        allTrips: [...prevState.allTrips, newTrip],
+      };
+    });
   }
 
   render() {
@@ -12,6 +31,14 @@ export class App extends React.Component {
       <Router>
         <div className="route-container">
           <Nav />
+          <Switch>
+            <Route
+              path="/add"
+              render={(props) => (
+                <AddTrip {...props} AddNewTrip={this.addTrip} />
+              )}
+            />
+          </Switch>
         </div>
       </Router>
     );
---
+++ src/components/trip/AddTrip.js
@@ -0,0 +1,53 @@
+import React from "react";
+
+export const AddTrip = ({ AddNewTrip }) => {
+  let date, place, type;
+  const submit = (e) => {
+    e.preventDefault();
+    const obj = {
+      date: date.value,
+      place: place.value,
+      type: type.value,
+    };
+    AddNewTrip(obj);
+    date.value = place.value = type.value = "";
+  };
+  return (
+    <div className="form-container">
+      <form onSubmit={submit} className="form black-container">
+        <label>
+          <h3>Add a trip</h3>
+          <br />
+          <br />
+          <br />
+        </label>
+        <label>
+          Date: <br />
+          <input
+            id="date"
+            type="date"
+            required
+            ref={(input) => (date = input)}
+          />
+        </label>
+        <label>
+          Place: <br />
+          <input
+            id="date"
+            type="text"
+            required
+            ref={(input) => (place = input)}
+          />
+        </label>
+        <label>
+          Type: <br />
+          <select ref={(input) => (type = input)}>
+            <option value="Trek">Trek</option>
+            <option value="Club">Club</option>
+          </select>
+        </label>
+        <input type="submit" value="SUBMIT" />
+      </form>
+    </div>
+  );
+};
